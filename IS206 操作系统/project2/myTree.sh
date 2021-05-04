c9f0a43d357369d5e3f63b7fa6fa8083c01ae899
lt974982407 <974982407@qq.com>
2021-05-04 11:19:19 +0800

    add readme

--- IS206 操作系统/project2/myTree.sh
+++ IS206 操作系统/project2/myTree.sh
@@ -1,14 +1,14 @@
-function myTree(){
-   for file in `ls $1`
+function myTree(){ #递归函数
+   for file in `ls $1` #对目录下的每一个文件、文件夹进行判断
    do
-      if [ -d "$1/$file" ]
+      if [ -d "$1/$file" ] #文件夹
       then
-         echo "$2$file"
-         myTree "$1/$file" "    $2"
-      else
-         echo "$2$file"
+         echo "$2$file" #输出名称
+         myTree "$1/$file" "    $2" #递归调用函数
+      else #文件
+         echo "$2$file" #输出名称
       fi
    done
 }
 thispath='./'
-myTree $thispath ""
+myTree $thispath "" #调用myTree函数
--- IS206 操作系统/project2/myfind.sh
+++ IS206 操作系统/project2/myfind.sh
@@ -1,23 +1,23 @@
-if [ $# -lt 1 ]
+if [ $# -lt 1 ] #输入参数个数小于1的情况
 then
    echo "The number of parameter cannot be less than 1."
    exit
 fi
 
-if [ $# -gt 2 ]
+if [ $# -gt 2 ] #输入参数个数大于2的情况
 then
    echo "The number of parameter cannot be more than 2."
    exit
 fi
 
-if [ $# -eq 1 ]
+if [ $# -eq 1 ] #仅输入一个pattern参数
 then
-   path=./
-   target=$1
-else
+   path=./ #在当前目录下寻找
+   pattern=$1
+else #输入两个参数
    path=$1
-   target=$2
+   pattern=$2
 fi
 
-find $path -name "*.[ch]" | xargs grep -Hn $target
+find $path -name "*.[ch]" | xargs grep -Hn $pattern #进行搜索和打印
 
